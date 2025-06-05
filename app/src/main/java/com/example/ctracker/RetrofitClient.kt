cdce7fa04e586b34ca969d8a4dc49ddb34d93311
SpriteCT <weqaetfs@gmail.com>
2025-06-05 20:01:43 +0300

    Changed server IP

--- app/src/main/java/com/example/ctracker/RetrofitClient.kt
+++ app/src/main/java/com/example/ctracker/RetrofitClient.kt
@@ -9,7 +9,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.8.0.1:5000"
+    private const val BASE_URL = "http://194.50.153.160:5000"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
